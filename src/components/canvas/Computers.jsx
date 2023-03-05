8a4d81406cd2dccc1822e56e11aa162aef54f63e
spirit-124 <bajrangofficial580@gmail.com>
2023-03-05 19:51:19 +0530

    [Added] stars and Earth camvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,7 @@
-import { useEffect, Suspense, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF, Preload } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
