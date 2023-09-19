9585043d02c10cae39f46bf0f9ba8176f6d9f1ca
spirit-124 <bajrangofficial580@gmail.com>
2023-09-19 17:51:44 +0530

    minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { motion } from "framer-motion";
+
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
@@ -20,12 +20,11 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop Frontend, Backend <br className="sm:block hidden" />
-            and Full Stack web applications
+            and Full Stack web applications.
           </p>
         </div>
       </div>
-
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
